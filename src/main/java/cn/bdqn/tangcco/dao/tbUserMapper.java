c5c3ded562e2fd295b9c796d6bb45a2c3b2c0d86
lifanfei <609987416@qq.com>
2017-08-05 09:09:49 +0800

    ddd

--- src/main/java/cn/bdqn/tangcco/dao/tbUserMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.bdqn.tangcco.dao;
-
-/**
- * Created by Administrator on 2017/8/4.
- */
-public interface tbUserMapper {
-}
--- src/main/java/cn/bdqn/tangcco/service/tbUserService.java
+++
@@ -1,7 +0,0 @@
-package cn.bdqn.tangcco.service;
-
-/**
- * Created by Administrator on 2017/8/4.
- */
-public interface tbUserService {
-}
